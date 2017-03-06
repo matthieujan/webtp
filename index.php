01a9746d59dd1598dadcfd6505aa19b04d82a144
matthieujan <matthieu.jan56@gmail.com>
2017-03-06 15:19:13 +0000

    tp3 p1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>WEB Work</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<link href="css/index.css" type="text/css" rel="stylesheet" />
+		<link href="/css/index.css" type="text/css" rel="stylesheet" />
 	</head>
 
 	<body>
