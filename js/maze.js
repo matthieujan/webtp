b4dd6562b67e3180bf8aa31d1afb03236c0c3bc1
Deimos <matthieu.jan56@gmail.com>
2017-03-06 14:49:31 +0100

    Test js

---
+++ js/maze.js
@@ -0,0 +1,9 @@
+var boundary1 = document.getElementById("boundary1");
+
+boundary1.onMouseOver = function(){
+    boundary1.style.color = "red";
+}
+
+boundary1.onMouseOut = function(){
+    boundary1.style.color = "grey";
+}
