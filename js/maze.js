bddd7d3d8a06e2cc86c1a75d3a80b5f3c38c39a1
Deimos <matthieu.jan56@gmail.com>
2017-03-06 14:58:31 +0100

    Test js

--- js/maze.js
+++ js/maze.js
@@ -1,9 +1,9 @@
 var boundary1 = document.getElementById("boundary1");
 
 boundary1.onMouseOver = function(){
-    alerte("JE MARCHE LOL");
+    boundary1.style.backgroundColor = "red";
 }
 
 boundary1.onMouseOut = function(){
-    boundary1.style.background-color = "grey";
+    boundary1.style.backgroundColor = "grey";
 }
