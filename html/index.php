a3d8de139802f115fa81c49246523b6433d7d3c8
matthieujan <matthieu.jan56@gmail.com>
2017-03-06 21:55:02 +0000

    Fix perm

---
+++ html/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html">
+<html>
+	<head>
+		<title>WEB Work</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="/css/index.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<div id="header">
+
+			<h1>WEB 101 - Matthieu Jan</h1>
+			<h2>List of the different Exercises</h2>
+		</div>
+
+		<div id="listarea">
+			<ul id="tdlist">
+				<li class="tditem">
+					<a href="html/td1_1.html">Td1 - partie 1</a>
+				</li>
+				<li class="tditem">
+					<a href="html/td1_2.html">Td1 - partie 2</a>
+				</li>
+                <li class="tditem">
+					<a href="html/td2_1.html">Td2 - partie 1</a>
+				</li>
+				<li class="tditem">
+					<a href="html/td2_2.html">Td2 - partie 2</a>
+				</li>
+                <li class="tditem">
+					<a href="html/td3_1.html">Td3 - partie 1</a>
+				</li>
+				<li class="tditem">
+					<a href="html/td3_2.html">Td3 - partie 2</a>
+                </li>
+            </ul>
+		</div>
+	</body>
+</html>
