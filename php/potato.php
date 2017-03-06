11ee5c175188ca94cf9568cdee52797f47943019
Deimos <matthieu.jan56@gmail.com>
2017-03-06 22:06:46 +0100

    Adding WEBServer

--- php/potato.php
+++ php/potato.php
@@ -2,7 +2,7 @@
 header("Content-type: text/plain");
 $potatoPath = "potato.txt";
 if (isset($POST['parameters'])){
-    file_put_contents($potatoPath,$POST['parameters'])
+    file_put_contents($potatoPath,$POST['parameters']);
 }else{
     $parts = file_get_contents($potatoPath);
     echo substr($parts,0,-1);
