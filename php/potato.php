678ff1cc9fd1bff00cfb6a8d0c056c1aa6eb49fc
Deimos <matthieu.jan56@gmail.com>
2017-03-06 21:29:09 +0100

    Adding WEBServer

--- php/potato.php
+++ php/potato.php
@@ -2,7 +2,7 @@
 header("Content-type: text/plain");
 echo "eyes";
 $path = "potato.txt";
-$file = fopen($path,"w");
+$file = fopen($path,"w") or die ("Unable to open file");
 fwrite($file,"Blabla");
 fclose($file);
 >
