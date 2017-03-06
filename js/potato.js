7bc40d32977c938051623a1bd80f38608e9261b3
Deimos <matthieu.jan56@gmail.com>
2017-03-06 20:30:38 +0100

    Adding toggle

--- js/potato.js
+++ js/potato.js
@@ -33,13 +33,9 @@ function ajaxGotState(ajax) {
 // toggles that accessory and sends the changes to the server
 function toggleAccessory() {
     // make the accessory appear / disappear ...
-        var img = document.getElementById(this.alt+"_image") ;
-        if(!this.checked){
-            img.style.display = "inline";
-        }else{
-            img.style.display = "none";
-        }
 
+
+    window.alert(this.alt);
     // save the state to the server using Ajax ...
 
 
