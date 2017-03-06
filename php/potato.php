53aa29beaa9bad1fb3889a6cb793cefc9fb3f9c1
Deimos <matthieu.jan56@gmail.com>
2017-03-06 21:33:49 +0100

    Adding WEBServer

--- php/potato.php
+++ php/potato.php
@@ -1,9 +1,9 @@
 <?php
 header("Content-type: text/plain");
-$potatoPath = "/php/potato.txt";
+$potatoPath = "potato.txt";
 if (isset($POST['parameters'])){
 }else{
-    $potatoFile = fopen($potatoPath,"r") or die ();
+    $potatoFile = fopen($potatoPath,"r") or die ("Unable to open file");
     echo fread($potatoFile,filesize($potatoPath));
     fclose($potatoFile);
 }
