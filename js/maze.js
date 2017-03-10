3071ae566b795ab5cda03663b9efbe8ca1639a75
Deimos <matthieu.jan56@gmail.com>
2017-03-10 13:41:44 +0100

    Adding branch for tpMaze

---
+++ js/maze.js
@@ -0,0 +1,63 @@
+var started = false;
+var lost = false;
+
+
+window.onload= function(){
+	var b = document.getElementsByClassName("boundary");
+	var i;
+	for(i = 0; i< b.length; i++){
+		b[i].observe("mouseover", bOnMouseOver);
+	}
+
+	var st = document.getElementById("status");
+	st.innerHTML = "Click on the S to begin";
+
+	var s = document.getElementById("start");
+	s.observe("click", sOnMouseClick);
+	var e = document.getElementById("end");
+	e.observe("mouseover", eOnMouseOver);
+};
+
+function sOnMouseClick(event){
+	window.started = true;
+	window.lost = false;
+	
+	var st = document.getElementById("status");
+	st.innerHTML = "GO !";
+	
+	var b = document.getElementsByClassName("boundary");
+	var i;
+	for(i = 0; i< b.length; i++){
+		b[i].style.backgroundColor = "#eeeeee";
+	}
+};
+
+function eOnMouseOver(event){
+	var st = document.getElementById("status");
+	if(window.started){
+		if(window.lost){
+			st.innerHTML = "You lost! Click S to try again";
+		}else{
+			st.innerHTML = "You win ! Click S to try again";
+		}
+		window.started = false;
+		window.lost = false;	
+		var b = document.getElementsByClassName("boundary");
+		var i;
+		for(i = 0; i< b.length; i++){
+			b[i].style.backgroundColor = "#eeeeee";
+		}
+	}
+};
+
+function bOnMouseOver(event){
+	if(window.started){
+		window.lost = true;
+		var b = document.getElementsByClassName("boundary");
+		var i;
+		for(i = 0; i< b.length; i++){
+			b[i].style.backgroundColor = "red";
+		}
+	}
+};
+
