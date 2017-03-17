76af517e9a48cb810bb1d1339d75f6ab49eac7e6
Deimos <matthieu.jan56@gmail.com>
2017-03-17 15:05:55 +0100

    init formTp

---
+++ sucker.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Buy Your Way to a Better Education!</title>
+		<link rel="stylesheet" href="css/buyagrade.css" />
+	</head>
+
+	<body>
+		<h1>Thanks, sucker!</h1>
+		<p>Your information has been recorded.</p>
+		<dl>
+			<dt>Name</dt>
+			<dd>???</dd>
+
+			<dt>Section</dt>
+			<dd>???</dd>
+
+			<dt>Credit Card</dt>
+			<dd>???</dd>
+		</dl>
+	</body>
+</html>
